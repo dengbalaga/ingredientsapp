bb31ede4bfc0535d005bdf59035cc72e484716c1
Deng Balaga <dengbalaga@gmail.com>
2019-11-26 16:59:34 +0800

    first commit
    first commit

---
+++ precache-manifest.930cb9b40af3d2c9390a2a80a718ce8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4078115d8d5d546b887e2d6ce17be09b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e335805ce7cd7e2f8ce8",
+    "url": "/static/css/main.4208fe7e.chunk.css"
+  },
+  {
+    "revision": "c4fac090ccd39f2dca03",
+    "url": "/static/js/2.cc561872.chunk.js"
+  },
+  {
+    "revision": "e335805ce7cd7e2f8ce8",
+    "url": "/static/js/main.216541a3.chunk.js"
+  },
+  {
+    "revision": "03f679471308dede1c28",
+    "url": "/static/js/runtime-main.0467868a.js"
+  }
+]);
